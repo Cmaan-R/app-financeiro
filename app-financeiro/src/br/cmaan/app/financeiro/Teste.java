f3209a70c913ffa7aefe871384d88b592fcf3e6a
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-11 11:03:56 -0300

    Update

---
+++ app-financeiro/src/br/cmaan/app/financeiro/Teste.java
@@ -0,0 +1,14 @@
+package br.cmaan.app.financeiro;
+
+import br.cmaan.app.calculo.Calculadora;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		Calculadora calculadora = new Calculadora();
+		
+		System.out.println(calculadora.soma(2 , 3 ,4));
+	}
+
+}
