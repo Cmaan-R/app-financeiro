fa2cb75eb57d450039871082d3bbd3b965833483
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-11 16:18:16 -0300

    Update

--- app-financeiro/src/br/cmaan/app/financeiro/Teste.java
+++ app-financeiro/src/br/cmaan/app/financeiro/Teste.java
@@ -1,6 +1,7 @@
 package br.cmaan.app.financeiro;
 
 import br.cmaan.app.calculo.Calculadora;
+import br.cmaan.app.calculo.interno.OperacoesAritmeticas;
 
 public class Teste {
 	
@@ -10,6 +11,8 @@ public class Teste {
 		
 		System.out.println(calc.soma(2 , 3 ,4));
 		
+			OperacoesAritmeticas op =new OperacoesAritmeticas();
+			System.out.println(op.soma(4 , 5 ,6 ));
 	}
 	
 	
